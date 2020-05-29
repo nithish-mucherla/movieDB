8655dffa0ceed1b8addc715a3995c1d2426de97b
Nithish Mucherla <53960760+nithish-mucherla@users.noreply.github.com>
2020-05-29 22:04:17 +0530

    Add php files

---
+++ movieDb/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div style="background-color:#343a40;margin-top:5%;padding:5%">
+        <center><div ><a href="#top" style="text-decoration:none; color:#343a40;background-color:white;padding:1%;" ><b>^ To The Top</b></a></div>
+        <div style="margin:5%;color:#F5F5F5;font-family:verdana">
+            <div style="font-size:28px;">Contact<br></div>
+            <div style="font-size:14px;margin-bottom:2%"> <em>We love you! visit again</em></div>
+            <div style="font-size:14px;"> Vellore, TamilNadu</div>
+          <!--  <div style="font-size:14px;">Phone: 8870665582 | 9550225822</div>
+            <div style="font-size:14px;">Email: mucherlasai.nithish2017@vitstudent.ac.in | potnuru.teja2017@vitstudent.ac.in</div>-->
+            <div style="font-size:14px;margin:2%"> <em>Adios!</em></div>
+        </div>
+        </center>
+    </div>
+</footer>
